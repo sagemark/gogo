eebd7a9b07ea037c42ad96f6c78406ddbb9b1ed8
“Mark <msage@gogoair.com>
2017-07-14 08:27:58 -0500

    Eureka clients

---
+++ spring-microservices-eureka-server/spring-microservices-eureka-client-2/src/main/java/com/oreilly/cloud/SpringMicroservicesEurekaClient2Application.java
@@ -0,0 +1,30 @@
+package com.oreilly.cloud;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringMicroservicesEurekaClient2Application {
+
+	@Autowired
+	private EurekaClient client;
+
+	@RequestMapping("/serviceinfo")
+	public String serviceInfo(){
+		InstanceInfo instance = client.getNextServerFromEureka("myClient", false);
+		return instance.getHomePageUrl();
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesEurekaClient2Application.class, args);
+	}
+}
