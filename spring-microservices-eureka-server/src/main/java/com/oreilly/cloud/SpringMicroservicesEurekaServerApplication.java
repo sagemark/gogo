4fd5fad59bcdc9dd69e89629e2858bd42aad0fcd
“Mark <msage@gogoair.com>
2017-07-13 21:20:32 -0500

    Eureka cluster

---
+++ spring-microservices-eureka-server/src/main/java/com/oreilly/cloud/SpringMicroservicesEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.oreilly.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringMicroservicesEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesEurekaServerApplication.class, args);
+	}
+}
